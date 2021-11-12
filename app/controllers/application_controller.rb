c9277de224ec89981f57bfcb743073719fb895d3
AlfredoJA <alfre.jangeles@hotmail.com>
2021-11-11 22:37:33 -0600

    Permit new attributes for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :first_name, :last_name, :email, :phone, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :phone, :password, :password_confirmation, :current_password])
+  end
 end
