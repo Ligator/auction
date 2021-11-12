921aec8a64ed6ffd52b7ceb4efe8b4fb1dc35f48
AlfredoJA <alfre.jangeles@hotmail.com>
2021-11-12 03:11:45 -0600

    Added import file to csv or xls for users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.xls { send_data @users.to_csv(col_sep: "\t") }
+      format.csv { send_data @users.to_csv }
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,19 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   # validates :admin, presence: true
   validates :phone, presence: true, length: { minimum: 10}
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      col_names = column_names.dup
+      col_names.delete("encrypted_password")
+      col_names.delete("reset_password_token")
+      col_names.delete("reset_password_sent_at")
+      col_names.delete("remember_created_at")
+      col_names.delete("admin")
+      csv << col_names
+      all.each do |product|
+        csv << product.attributes.values_at(*col_names)
+      end
+    end
+  end
 end
