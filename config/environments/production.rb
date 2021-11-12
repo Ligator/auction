1c0b254f1a8dda7da29bd21ce3da663df9b9a341
Abel Pacheco <abelpacheco@outlook.com>
2021-11-12 06:23:49 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,12 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-   config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i'
-  # }
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'support@colordemipueblo.com'}
