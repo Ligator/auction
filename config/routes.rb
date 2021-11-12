9991f7a3535e8164fa9fdd2ab856cc817cd2e299
davepcage <71347825+davepcage@users.noreply.github.com>
2021-11-12 00:26:48 -0600

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'statics/coming'
   devise_for :users
   resources :products
-  root "products#index"
+  root "statics#coming"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
