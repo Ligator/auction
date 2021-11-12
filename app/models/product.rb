21d4bb2556ee79f2ba2fb0c74b764788e7d0b340
Abel Pacheco <abelpacheco@outlook.com>
2021-11-12 09:36:41 -0600

    Fix bug for suggested bid amount

--- app/models/product.rb
+++ app/models/product.rb
@@ -27,6 +27,6 @@ class Product < ApplicationRecord
 
   def next_bid_amount
     next_bid_amount = (max_bid_amount.to_d / ROUND_FACTOR).ceil * ROUND_FACTOR
-    next_bid_amount == max_bid_amount ? max_bid_amount + ROUND_FACTOR : max_bid_amount
+    next_bid_amount == max_bid_amount ? max_bid_amount + ROUND_FACTOR : next_bid_amount
   end
 end
