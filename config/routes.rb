b9fb9539ac22235bbe19dd142abd53b5c581b5cc
AlfredoJA <alfre.jangeles@hotmail.com>
2021-11-12 01:53:06 -0600

    Update config/routes.rb
    Co-authored-by: Abel Pacheco <abelpacheco@outlook.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :bids, only: [:index, :create]
   root "products#index"
 
-  resources :users
+  resources :users, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
