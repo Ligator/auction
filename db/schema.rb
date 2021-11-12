acba170e7fcfc454e72a8345c92698cbfff98899
Abel Pacheco <abelpacheco@outlook.com>
2021-11-12 00:58:57 -0600

    Adding Bids

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_040045) do
+ActiveRecord::Schema.define(version: 2021_11_12_065112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bids", force: :cascade do |t|
+    t.decimal "amount"
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_bids_on_product_id"
+    t.index ["user_id"], name: "index_bids_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -38,4 +48,6 @@ ActiveRecord::Schema.define(version: 2021_11_12_040045) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bids", "products"
+  add_foreign_key "bids", "users"
 end
