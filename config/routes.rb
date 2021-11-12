7a4a01e04f04ea6f53f13369739cef2d4469f647
Abel Pacheco <abelpacheco@outlook.com>
2021-11-12 02:00:17 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: :index
   resources :products
   resources :bids, only: [:index, :create]
-  root "products#index"
+  root "bids#index"
 
-  resources :users, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
