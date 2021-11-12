5e3c2f35338e57a81039ea6b6f3b7e93c8fa8226
AlfredoJA <alfre.jangeles@hotmail.com>
2021-11-12 02:39:12 -0600

    Update time zone

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Auction
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.time_zone = "Mexico City"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
